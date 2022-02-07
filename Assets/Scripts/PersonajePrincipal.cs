a717b5466b677166092f2bb3e84873200ae8b4bb
Julianlo3 <94806383+Julianlo3@users.noreply.github.com>
2022-02-06 22:02:01 -0500

    Con animacion
    Ahora el personaje tiene animación de correr

--- Assets/Scripts/PersonajePrincipal.cs
+++ Assets/Scripts/PersonajePrincipal.cs
@@ -6,20 +6,44 @@ public class PersonajePrincipal : MonoBehaviour
 {
     public float velocidad; // variable para ponerle velocidad al personaje
     public float direccionX; // variable para usar el método para que el personaje se mueva en el eje X
+    private Rigidbody2D rigidbody2D; // variable para las fisicas del personaje
+    private Animator animator; // variable para controlar la animacion
+    public float fuerzaSalto; // variable para la fuerza del salto
 
-
-    // Start is called before the first frame update
     void Start()
     {
-       // se inicializa la velocidad al iniciar el juego
+        velocidad = 0.1f;// se inicializa la velocidad al iniciar el juego
+        fuerzaSalto = 150; 
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         direccionX = Input.GetAxis("Horizontal");
 
         // transform.position += new Vector3(direccionX * velocidad, 0, 0); //método para que el personaje se mueva
         transform.Translate(Vector3.right * direccionX * velocidad); // método para que el personaje se mueva
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Saltar();
+            animator.SetBool("AccionSaltar",true);
+            
+        }
     }
+
+    public void Saltar()
+    {
+        rigidbody2D.AddForce(Vector2.up * fuerzaSalto);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo")
+        {
+            animator.SetBool("AccionSaltar", false);
+        }
+    }
+
 }
