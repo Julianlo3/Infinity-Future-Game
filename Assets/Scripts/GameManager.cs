4e953ab3c7fde1521981af5d52a81b44a22548c1
Julianlo3 <94806383+Julianlo3@users.noreply.github.com>
2022-02-15 21:31:54 -0500

    Obstaculos
    obstaculos, animación de muerte y menú de game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,55 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    //--------------------
-    public Renderer Fondo; // La  variable que se usa para manejar el fondo
-    public float velocidadFondo;
-    public GameObject Suelo;
-    public GameObject Techo;
-    public List<GameObject> Suelos;
-    public List<GameObject> Techos;
-    //----------------------
-    // Start is called before the first frame update
-    void Start()
-    {
-        // crear suelo
-        for (int i = 0; i < 21; i++)
-        {
-            Suelos.Add(Instantiate(Suelo, new Vector2(-10 + i, -5), Quaternion.identity));
-        }
-        // crear techo
-        for (int i = 0; i < 2; i++)
-        {
-            Techos.Add(Instantiate(Techo, new Vector2(0.5f + i, 8.5f), Quaternion.identity));
-        }
-    }
+	//--------------------
+	public Renderer Fondo; // La  variable que se usa para manejar el fondo
+	public float velocidadFondo;
+	public GameObject Suelo;
+	public GameObject Techo;
+	public GameObject Policia;
+	public List<GameObject> Suelos;
+	public List<GameObject> Techos;
+	public List<GameObject> Policias;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Fondo.material.mainTextureOffset = Fondo.material.mainTextureOffset + new Vector2(velocidadFondo, 0) * Time.deltaTime;
+	public GameObject MenuGameOver;
+	public bool gameOver = false;
+	public bool start = false;
 
-        // mover mapa
-        for (int i = 0; i < Suelos.Count; i++)
-        {
-            if (Suelos[i].transform.position.x <= -10)
-            {
-                Suelos[i].transform.position = new Vector3(10, -5, 0);
-            }
-            Suelos[i].transform.position = Suelos[i].transform.position + new Vector3(-1,0,0) * Time.deltaTime * 2;
-        }
-        for (int i = 0; i < Techos.Count; i++)
-        {
-            if (Techos[i].transform.position.x <= -3)
-            {
-                Techos[i].transform.position = new Vector3(0.5f, 8.5f, 0);
-            }
-            Techos[i].transform.position = Techos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * 2;
-        }
+	//----------------------
+	// Start is called before the first frame update
+	void Start()
+	{
+		// crear suelo
+		for (int i = 0; i < 21; i++)
+		{
+			Suelos.Add(Instantiate(Suelo, new Vector2(-10 + i, -5), Quaternion.identity));
+		}
+		// crear techo
+		for (int i = 0; i < 2; i++)
+		{
+			Techos.Add(Instantiate(Techo, new Vector2(0.5f + i, 8.5f), Quaternion.identity));
+		}
+		// crear policias
+		for (int i = 0; i < 1; i++)
+		{
+			Policias.Add(Instantiate(Policia, new Vector2(12 + i, 6), Quaternion.identity));
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		start = true;
+
+		if (gameOver == true)
+		{
+			MenuGameOver.SetActive(true);
+			if (Input.GetKeyDown(KeyCode.X))
+			{
+				SceneManager.LoadScene(1);
+			}
+		}
 
 
-    }
+		if (start == true && gameOver == false)
+		{
+
+			Fondo.material.mainTextureOffset = Fondo.material.mainTextureOffset + new Vector2(velocidadFondo, 0) * Time.deltaTime;
+
+			// mover mapa
+			for (int i = 0; i < Suelos.Count; i++)
+			{
+				if (Suelos[i].transform.position.x <= -10)
+				{
+					Suelos[i].transform.position = new Vector3(10, -5, 0);
+				}
+				Suelos[i].transform.position = Suelos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * 2;
+			}
+			for (int i = 0; i < Techos.Count; i++)
+			{
+				if (Techos[i].transform.position.x <= -3)
+				{
+					Techos[i].transform.position = new Vector3(0.5f, 8.5f, 0);
+				}
+				Techos[i].transform.position = Techos[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * 2;
+			}
+
+			//Obstaculos (policias)
+			for (int i = 0; i < Policias.Count; i++)
+			{
+				if (Policias[i].transform.position.x <= -15)
+				{
+					PersonajePrincipal personaje = new PersonajePrincipal();
+
+					float randomx = Random.Range(11, 18);
+					float randomy = Random.Range(-4, 4);
+					Policias[i].transform.position = new Vector3(randomx, randomy, 0);
+				}
+				Policias[i].transform.position = Policias[i].transform.position + new Vector3(-1, 0, 0) * Time.deltaTime * 11;
+			}
+		}
+	}
 }
--- Assets/Scripts/PersonajePrincipal.cs
+++ Assets/Scripts/PersonajePrincipal.cs
@@ -4,34 +4,27 @@ using UnityEngine;
 
 public class PersonajePrincipal : MonoBehaviour
 {
-    public float velocidad; // variable para ponerle velocidad al personaje
-    public float direccionX; // variable para usar el método para que el personaje se mueva en el eje X
     private Rigidbody2D rigidbody2D; // variable para las fisicas del personaje
     private Animator animator; // variable para controlar la animacion
     public float fuerzaSalto; // variable para la fuerza del salto
+    public GameManager gameManager;
+    Vector3 posicionInicial;
 
     void Start()
     {
-        velocidad = 0.1f;// se inicializa la velocidad al iniciar el juego
-        fuerzaSalto = 150; 
+        fuerzaSalto = 150;
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        transform.position=(new Vector3(-7.64f, -3.36f, 0));
+        transform.position = (new Vector3(-7.64f, -3.36f, 0));
     }
 
-   
+
     void Update()
     {
-       // direccionX = Input.GetAxis("Horizontal");
-
-        // transform.position += new Vector3(direccionX * velocidad, 0, 0); //método para que el personaje se mueva
-        //transform.Translate(Vector3.right * direccionX * velocidad); // método para que el personaje se mueva
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) &&  gameManager.gameOver == false)
         {
             Saltar();
             animator.SetBool("AccionSaltar", true);
-            
         }
     }
 
@@ -46,6 +39,16 @@ public class PersonajePrincipal : MonoBehaviour
         {
             animator.SetBool("AccionSaltar", false);
         }
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            gameManager.gameOver = true;
+            animator.SetBool("Muerte", true);
+        }
+    }
+
+    public Vector3 getPosicion()
+    {
+        return posicionInicial;
     }
 
 }
